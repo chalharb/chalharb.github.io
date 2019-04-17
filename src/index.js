019e0e52c4f80899181dcc49d6733a1d029d3101
Logan Harber <chalharb@gmail.com>
2019-04-17 00:17:14 -0500

    outlined basic DOM structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,34 @@
 require('normalize.css/normalize.css');
 require('./styles/index.scss');
 
-console.log("JS has been loaded.");
+function isInView() {
+
+}
+
+window.onload = function () {
+    let navLinks = document.querySelectorAll("#sidebar-navigation a");
+    let section = document.querySelectorAll(".main-section");
+    let sections = {};
+    let i = 0;
+
+    // console.log(navLinks[0].getAttribute("href"));
+
+  Array.prototype.forEach.call(section, function(e) {
+      console.log(e.offsetTop);
+    sections[e.id] = e.offsetTop;
+  });
+
+  console.log(sections);
+
+   window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    console.log(scrollPosition);
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelector('.is-active')[0].setAttribute('class', ' ');
+        document.querySelector('a[href*=' + i + ']').setAttribute('class', 'active');
+      }
+    }
+  };
+};
